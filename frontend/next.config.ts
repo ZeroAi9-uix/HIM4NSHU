4ed06db78daf2c12a1ac2c4b496bfbceb60d792a
ZeroAi9-uix <gg5624himanshupatel15@gmail.com>
2026-06-08 14:37:59 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Allow dev origins for LAN/network access
-  allowedDevOrigins: ["192.168.1.5", "localhost", "unintimidated-connately-merrilee.ngrok-free.dev"],
+  allowedDevOrigins: ["192.168.1.5", "localhost", "unintimidated-connately-merrilee.ngrok-free.dev","https://him-4-nshu.vercel.app/","https://him4nshu.onrender.com"],
 
   // Environment variables available to the browser
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://him4nshu.onrender.com/api',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://him4nshu.onrender.com',
   }
 
 };
