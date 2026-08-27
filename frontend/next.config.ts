6ecc1c24e7db301f32a7de3fd217f1b7365daa46
ZeroAi9-uix <gg5624himanshupatel15@gmail.com>
2026-08-27 10:36:08 +0530

    Add new allowedDevOrigin for backend access

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Allow dev origins for LAN/network access
-  allowedDevOrigins: ["192.168.1.5", "localhost", "unintimidated-connately-merrilee.ngrok-free.dev","https://him-4-nshu.vercel.app/","https://him4nshu.onrender.com"],
+  allowedDevOrigins: ["192.168.1.5", "localhost", "unintimidated-connately-merrilee.ngrok-free.dev","https://him-4-nshu.vercel.app/","https://him4nshu.onrender.com","https://backend-hnra.onrender.com"],
 
   // Environment variables available to the browser
   env: {
